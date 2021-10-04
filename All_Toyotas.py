5bf025c37af3f44b7067ca17c34464347dda5e49
jdavidbd <39505229+jdavidbd@users.noreply.github.com>
2021-10-04 16:47:16 -0400

    Add files via upload

---
+++ All_Toyotas.py
@@ -0,0 +1,24 @@
+from typing import Counter
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "https://www.supercarros.com/carros/cualquier-tipo/cualquier-provincia/Toyota/?PagingPageSkip=41"
+page = requests.get(url).text
+doc = BeautifulSoup(page, "html.parser")
+
+page_text = doc.find(id="bigsearch-results-inner-lowerbar-pages")
+pages = int(str(page_text).split("40")[-1].split("a")[0].split(">")[-1][:2])
+
+for page in range(0, pages+1):
+    url = f"https://www.supercarros.com/carros/cualquier-tipo/cualquier-provincia/Toyota/?PagingPageSkip={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+
+    busqueda = doc.find_all(class_="title1")
+    modelo = list()
+
+    for i in busqueda:
+        modelo.append(i.text)
+        print(modelo)
+        
